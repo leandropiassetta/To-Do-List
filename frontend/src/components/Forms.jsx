02eeb27ebf2e45b93194c1d794e1753bae06853e
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-11-05 00:25:44 -0300

    iniciando frontend

---
+++ frontend/src/components/Forms.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Forms() {
+  return (
+    <form method="POST">
+      <label htmlFor="input-task">
+        <input
+        id="input-task"
+        type="text" 
+        placeholder="Adicione uma tarefa"
+        name="task"
+        onChange={ handleChange }
+        />
+      </label>
+      <button onClick={ handleClick }> Adicionar tarefa</button>
+    </form>
+  );
+}
+
+export default Forms;
