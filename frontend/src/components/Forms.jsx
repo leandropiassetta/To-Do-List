c1f2b2d53210f6e373c017b805cbb3c986c5d7a4
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-11-08 16:20:10 -0300

    implementando a funcionalidade de criar tarefas na aplicaçao'

--- frontend/src/components/Forms.jsx
+++ frontend/src/components/Forms.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-
-// handleChange(e) {
-
-// }
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import axiosApi from '../api/request';
 
 function Forms() {
+  const [tasks, setTasks] = useState([]);
+  const dispatch = useDispatch();
+
+  const handleChange = ({ target: { value } }) => setTasks(value);
+
+  const handleClick = (e) => {
+    e.preventDefault() // para não atualizar depois de uma modificação na página
+
+    axiosApi.post('/', { task: tasks }); // segundo argumento é o req.body
+
+    dispatch({
+      type: 'sendTask',
+      tasks: true
+    });
+
+    setTasks('');
+  }
+
   return (
     <form method="POST">
       <label htmlFor="input-task">
@@ -12,11 +28,13 @@ function Forms() {
         id="input-task"
         type="text" 
         placeholder="Adicione uma tarefa"
+        value={ tasks }
         name="task"
-        // onChange={ handleChange }
+        onChange={ handleChange }
+        required
         />
       </label>
-      <button /* onClick={ handleClick } */> Adicionar tarefa</button>
+      <button onClick={ handleClick } > Adicionar tarefa</button>
     </form>
   );
 }
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,23 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { connect } from 'react-redux';
 import axiosApi from '../api/request';
 
 function CreateList() {
-
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    axiosApi.get('/')
-    .then((task) => setTasks(task.data))
-    .catch((error) => `Ocorreu um erro chamado: ${error}`)
-  }, []);
-
+  
+  const [tasks, setTasks] = useState([]);gst
+  
+  const getTasks = useCallback(
+    () => {
+      axiosApi.get('/')
+      .then((task) => setTasks(task.data))
+      .catch((error) => `Ocorreu um erro chamado: ${error}`)
+    }, []);
+    
+    useEffect(() => {
+      getTasks();
+    }, [getTasks, tasks]);
+    
   return (
     <ul>
       {tasks.map(({task}, index) => <li key={index}>
         { task }
       </li>)}
     </ul>
-  )
+  );
 }
 
-export default CreateList;
+const mapStateToProps = (state) => (
+  state => ({
+    task: state.task
+  })
+);
+
+export default connect(mapStateToProps)(CreateList);
